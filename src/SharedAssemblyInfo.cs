c7f72c75121b8c65c4bdf78e9bb7f0d0c4339a56
Antony Briggs <abriggs@macmillan.org.uk>
2024-04-25 14:09:58 +0100

    Just a change to indicate the dll version

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Unicorn")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.1.6.0")]
-[assembly: AssemblyFileVersion("4.1.6.0")]
-[assembly: AssemblyInformationalVersion("4.1.6")]
+[assembly: AssemblyVersion("4.1.6.1")]
+[assembly: AssemblyFileVersion("4.1.6.1")]
+[assembly: AssemblyInformationalVersion("4.1.6-Issue429")]
 [assembly: CLSCompliant(false)]
